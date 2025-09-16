edb520ea5fbffca28c87f4324b79c6da3b8ea1c3
Thomas <thomas.rousselin@mdpqualite.fr>
2025-09-16 16:39:48 +0200

    Ajout pipe de transformation du numéro

---
+++ tuto_front/src/tools/pipes/display-french-number-pipe.spec.ts
@@ -0,0 +1,8 @@
+import { DisplayFrenchNumberPipe } from './display-french-number-pipe';
+
+describe('DisplayFrenchNumberPipe', () => {
+  it('create an instance', () => {
+    const pipe = new DisplayFrenchNumberPipe();
+    expect(pipe).toBeTruthy();
+  });
+});
