6ff6536345115da3c48fb64560685452cd5dcca7
Thomas <thomas.rousselin@mdpqualite.fr>
2025-09-16 15:19:51 +0200

    Ajout du menu

--- tuto_front/src/app/app.routes.ts
+++ tuto_front/src/app/app.routes.ts
@@ -1,7 +1,12 @@
 import { Routes } from '@angular/router';
 import { ListContact } from '../features/contacts/components/list-contact/list-contact';
+import { Home } from '../features/home/components/home';
 
 export const routes: Routes = [
+    {
+        path: '',
+        component: Home
+    },
     {
         path: 'contacts',
         component: ListContact
