792073f13f80d5b2133ae87d7724dbcb0ce5a744
rousselin thomas <thomas.rousselin@mdpqualite.fr>
2025-09-16 09:14:19 +0200

    ajout composant list

---
+++ tuto_front/src/features/contacts/components/list-contact/list-contact.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import {MatListModule} from '@angular/material/list';
+
+@Component({
+  selector: 'app-list-contact',
+  imports: [MatListModule],
+  templateUrl: './list-contact.html',
+  styleUrl: './list-contact.css'
+})
+export class ListContact {
+
+}
